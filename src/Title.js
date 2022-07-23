7210969f713360233a99ac627985301cc7a63286
nemariia <lusuwey@gmail.com>
2022-07-23 21:28:48 +0300

    improved title render

--- src/Title.js
+++ src/Title.js
@@ -1,18 +1,11 @@
 import './styles/title.css';
 
 function Title() {
-	const title = "как жить".split("");
+	const title = "как жить".split("").map((l, i) => <p key={i}>{l}</p>);
 	
 	return (
 		<h1 className="title">
-			<p>К</p>
-            <p>а</p>
-            <p>к</p>
-            <p> </p>
-            <p>ж</p>
-            <p>и</p>
-            <p>т</p>
-            <p>ь</p>
+			{title}
         </h1>
 	);
 }
