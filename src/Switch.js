689002b67e56d9cfaea88f6690a1ef06635ae7c5
nemariia <lusuwey@gmail.com>
2022-07-23 16:35:33 +0300

    added click handling logic to switch

--- src/Switch.js
+++ src/Switch.js
@@ -4,10 +4,25 @@ import './styles/textslider.css';
 class Switch extends React.Component {
 	constructor(props) {
 		super(props);
+		this.handleClick = this.handleClick.bind(this);
+	}
+
+	handleClick() {
+		if(this.props.direction == "previous") {
+			console.log("must show previous text");
+		}
+		else if(this.props.direction == "next") {
+			console.log("must show next text");
+		}
+		else {
+			console.log("something unexpected just happened");
+		}
 	}
 
 	render() {
-		return <button className={this.props.direction == "next"? "switch switch_next" : "switch"}></button>;
+		return <button
+			className={this.props.direction == "next"? "switch switch_next" : "switch"}
+			onClick={this.handleClick}></button>;
 	}
 }
 
