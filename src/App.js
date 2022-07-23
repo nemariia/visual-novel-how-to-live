f5be3e998c1c756ad4cee0d25413e840eab59add
nemariia <lusuwey@gmail.com>
2022-07-23 13:56:27 +0300

    added menu buttons to the main screen

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
 import Title from './Title.js';
+import Button from './Button.js';
 
 function App() {
   return (
     <main className="App">
       <Title />
+      <Button text="играть" />
+      <Button text="авторы" />
+      <Button text="музыка" />
     </main>
   );
 }
