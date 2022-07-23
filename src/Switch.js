5d6ec7da715ca21cdef72d3519af7623c28c2b67
nemariia <lusuwey@gmail.com>
2022-07-23 15:30:56 +0300

    added switch component with styles

---
+++ src/Switch.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './styles/textslider.css';
+
+class Switch extends React.Component {
+	constructor(props) {
+		super(props);
+	}
+
+	render() {
+		return <button className="switch"></button>;
+	}
+}
+
+export default Switch;
