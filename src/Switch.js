b8b3cfb7e3daab5ac9493952396373603deaaecf
nemariia <lusuwey@gmail.com>
2022-07-23 19:05:54 +0300

    moved click handling logic form switch to textslider

--- src/Switch.js
+++ src/Switch.js
@@ -4,25 +4,12 @@ import './styles/textslider.css';
 class Switch extends React.Component {
 	constructor(props) {
 		super(props);
-		this.handleClick = this.handleClick.bind(this);
-	}
-
-	handleClick() {
-		if(this.props.direction == "previous") {
-			console.log("must show previous text");
-		}
-		else if(this.props.direction == "next") {
-			console.log("must show next text");
-		}
-		else {
-			console.log("something unexpected just happened");
-		}
 	}
 
 	render() {
 		return <button
 			className={this.props.direction == "next"? "switch switch_next" : "switch"}
-			onClick={this.handleClick}></button>;
+			onClick={this.props.onClick}></button>;
 	}
 }
 
--- src/TextSlider.js
+++ src/TextSlider.js
@@ -5,13 +5,27 @@ import './styles/textslider.css';
 class TextSlider extends React.Component {
 	constructor(props) {
 		super(props);
+		this.handleClick = this.handleClick.bind(this);
+	}
+
+	handleClick(e) {
+		if(e.target.classList.contains("switch_next")) {
+			console.log("must show next text");
+		}
+		else if(!e.target.classList.contains("switch_next")) {
+			console.log("must show previous text");
+		}
+		else {
+			console.log("something unexpected just happened");
+		}
 	}
 
 	render() {
 		return (
 			<section>
-			<Switch direction="previous"/>
-			<Switch direction="next"/>
+				<p className="text"></p>
+				<Switch onClick={this.handleClick} direction="previous"/>
+				<Switch onClick={this.handleClick} direction="next"/>
 			</section>
 		)
 	}
